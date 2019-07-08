a0561f120bcd097e6c88224cb0c4b8b269edd7aa
Ade Wiranata Putra <adewiranataputra@gmai.com>
2019-07-08 13:19:11 +0700

    vue route

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,20 @@
 import '../styles/main.scss';
-import readme from '../../README.md';
-document.getElementById('readme').innerHTML = readme;
+import Vue from 'vue'
+import App from './vue/app.vue'
+import { createRouter } from './router'
+
+export function createApp() {
+	const router = createRouter();
+
+	const app = new Vue({
+		el: '#app',
+		router,
+		render: h => h(App),
+		components: { app }
+	});
+
+	// expose the app and the router
+	return { app, router };
+}
+
+window.onload = () => createApp();
