28f5f9e691642ff87f250c5a311d2298665c271a
Ade Wiranata Putra <adewiranataputra@gmail.com>
2019-07-10 14:20:57 +0700

    pagination

--- src/scripts/vue/components/table.vue
+++ src/scripts/vue/components/table.vue
@@ -1,9 +1,39 @@
 <template>
-	<b-table :data="data" :columns="fields"></b-table>
+	<section>
+		<b-field>
+			<b-input placeholder="Search..." type="search" icon="magnify">
+			</b-input>
+		</b-field>
+		<b-table
+			:paginated="isPaginated"
+			:per-page="perPage"
+			:current-page.sync="currentPage"
+			:pagination-simple="isPaginationSimple"
+			:pagination-position="paginationPosition"
+			:default-sort-direction="defaultSortDirection"
+			default-sort="user.first_name"
+			aria-next-label="Next page"
+			aria-previous-label="Previous page"
+			aria-page-label="Page"
+			aria-current-label="Current page"
+			:data="data"
+			:columns="fields"
+		></b-table>
+	</section>
 </template>
 
 <script>
 export default {
+	data() {
+		return {
+			isPaginated: true,
+			isPaginationSimple: true,
+			paginationPosition: "bottom",
+			defaultSortDirection: "asc",
+			currentPage: 1,
+			perPage: 5
+		};
+	},
 	props: {
 		fields: {
 			type: Object,
