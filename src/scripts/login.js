5ce96247d21ae7023cd1996d47b41adddea05443
Ade Wiranata Putra <adewiranataputra@gmail.com>
2019-07-10 17:04:35 +0700

    add search table

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
 import controller from './vue/pages/loginController.vue';
-import '../styles/main.scss';
+// import '../styles/main.scss';
 
 new Vue(controller).$mount('#vapp');
--- src/scripts/vue/components/table.vue
+++ src/scripts/vue/components/table.vue
@@ -1,7 +1,12 @@
 <template>
 	<section>
 		<b-field>
-			<b-input placeholder="Search..." type="search" icon="magnify">
+			<b-input
+				placeholder="Search..."
+				type="search"
+				icon="magnify"
+				v-model="search"
+			>
 			</b-input>
 		</b-field>
 		<b-table
@@ -11,12 +16,11 @@
 			:pagination-simple="isPaginationSimple"
 			:pagination-position="paginationPosition"
 			:default-sort-direction="defaultSortDirection"
-			default-sort="user.first_name"
 			aria-next-label="Next page"
 			aria-previous-label="Previous page"
 			aria-page-label="Page"
 			aria-current-label="Current page"
-			:data="data"
+			:data="filteredlist"
 			:columns="fields"
 		></b-table>
 	</section>
@@ -31,22 +35,53 @@ export default {
 			paginationPosition: "bottom",
 			defaultSortDirection: "asc",
 			currentPage: 1,
-			perPage: 5
+			perPage: 5,
+			search: ""
 		};
 	},
 	props: {
 		fields: {
-			type: Object,
+			type: Array,
 			required: true
 		},
 		data: {
-			type: Object,
+			type: Array,
 			required: true
 		}
 	},
 	methods: {
 		searching() {
 			return NULL;
+		}
+	},
+	computed: {
+		filteredlist() {
+			let self = this;
+			return self.data.filter(post => {
+				let iwoFilter = post.iwo
+					.toLowerCase()
+					.includes(self.search.toLowerCase());
+				let projectNameFilter = post.project_name
+					.toLowerCase()
+					.includes(self.search.toLowerCase());
+				let pmFilter = post.pm
+					.toLowerCase()
+					.includes(self.search.toLowerCase());
+				let statusFilter = post.status
+					.toLowerCase()
+					.includes(self.search.toLowerCase());
+				let progressFilter = post.progress
+					.toLowerCase()
+					.includes(self.search.toLowerCase());
+
+				return (
+					iwoFilter ||
+					projectNameFilter ||
+					pmFilter ||
+					statusFilter ||
+					progressFilter
+				);
+			});
 		}
 	}
 };
