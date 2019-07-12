5feb33600ece7498c4ef3d0f83fc56b0b6178fa0
adewiranataputra <adewiranataputra@gmail.com>
2019-07-12 11:22:46 +0700

    add ganttchart to master

---
+++ src/scripts/vue/pages/chart.vue
@@ -0,0 +1,18 @@
+<template>
+	<v-chart :dataBaru="dataBaru"></v-chart>
+</template>
+
+<script>
+import vChart from "../components/ganttchart";
+
+export default {
+	components: {
+		vChart
+	},
+	data() {
+		return {
+			dataBaru: DATA
+		};
+	}
+};
+</script>
