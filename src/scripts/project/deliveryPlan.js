ea6744d95714099fb5d1f5a815ddf2451ee77f57
Ario Widiatmoko <ariowidiatmoko@gmail.com>
2019-08-14 10:56:47 +0700

    Update Delivery Plan

--- src/scripts/project/deliveryPlan.js
+++ src/scripts/project/deliveryPlan.js
@@ -1,10 +1,17 @@
 import Vue from 'vue';
 import Buefy from 'buefy';
+import Notified from '../vue/components/notified.vue';
+import FormUpload from '../../scripts/vue/pages/projects/deliveryFileForm.vue';
 
 Vue.use(Buefy);
 new Vue({
 	el: '#app',
+	components: { FormUpload, Notified },
 	data: {
+		top: {
+			status: false,
+			periode: []
+		},
 		delivery: [],
 		planField: [
 			{
@@ -16,7 +23,32 @@ new Vue({
 				field: 'plan',
 				label: '🔋 Progress',
 			}
-		]
+		],
+		formModal: {
+			display: false,
+			doctype: DOCTYPE,
+			delivery: {
+				periode: "",
+				plan: "",
+				actual: null
+			}
+		}
+	},
+	methods: {
+		notesChecker(notes){
+			if(notes === undefined || notes === ""){
+				return "tidak ada pesan";
+			}else{
+				return notes;
+			}
+		},
+		tooltipIsMultilined(notes){
+			return notes !== undefined && notes.length > 28;
+		},
+		displayForm(delivery){
+			this.formModal.delivery = delivery;
+			this.formModal.display = true;
+		}
 	},
 	mounted(){
 		let firstNull = true;
@@ -29,6 +61,11 @@ new Vue({
 				}else{
 					row.showUpload = false;
 				}
+			}
+
+			if (row.topStatus !== undefined && row.topStatus === true){
+				this.top.periode.push(row.periode);
+				this.top.status = true;
 			}
 
 			this.delivery.push(row);
---
+++ src/scripts/vue/pages/projects/deliveryFileForm.vue
@@ -0,0 +1,224 @@
+<template>
+	<div class="modal-card">
+		<header class="modal-card-head">
+			<p class="modal-card-title">📝 Document Delivery Form</p>
+		</header>
+		<section class="modal-card-body">
+			<form :action="action" method="post" enctype="multipart/form-data">
+				<div class="columns">
+					<div class="column">
+						<p class="heading">Periode</p>
+						<b-field>
+							<b-input
+								class="has-text-white"
+								placeholder="Disabled"
+								v-model="delivery.periode"
+								disabled
+							></b-input>
+						</b-field>
+					</div>
+					<div v-if="checkVersion()" class="column">
+						<p class="heading">Versi</p>
+						<b-input
+							class="has-text-white"
+							placeholder="Disabled"
+							v-model="delivery.actual.version"
+							disabled
+						></b-input>
+					</div>
+				</div>
+				<div class="columns">
+					<div class="column">
+						<p class="heading">Document Type</p>
+						<b-field>
+							<b-select
+								placeholder="Pilih Tipe Dokumen"
+								name="docType"
+								required
+								expanded
+							>
+								<option
+									v-for="(option, index) in doctype"
+									:value="option"
+									:key="index"
+								>
+									{{ option }}
+								</option>
+							</b-select>
+						</b-field>
+					</div>
+					<div class="column">
+						<p class="heading">Progress (%)</p>
+						<b-field>
+							<b-numberinput
+								min="0"
+								max="100"
+								expanded
+								required
+								name="progress"
+								type="is-info"
+								v-model="progress"
+								controls-position="compact"
+							></b-numberinput>
+						</b-field>
+					</div>
+				</div>
+				<p class="heading">Document</p>
+				<div class="columns">
+					<div class="column">
+						<b-field
+							:type="determineFieldStatus"
+							message="File yang dibolehkan: jpg, jpeg, png, doc, docx,
+							pdf, xls, xlsx, ppt, pptx"
+						>
+							<b-input readonly v-model="documentName"></b-input>
+						</b-field>
+					</div>
+					<div class="column is-one-third">
+						<b-field>
+							<b-upload
+								v-model="selectedDocument"
+								name="evidDoc"
+								native
+								required
+								style="width:100%;"
+							>
+								<a class="button is-info is-fullwidth">
+									<b-icon icon="upload"></b-icon>
+									<span>Browse File</span>
+								</a>
+							</b-upload>
+						</b-field>
+					</div>
+				</div>
+				<p class="heading">Notes</p>
+				<b-field>
+					<b-input
+						name="notes"
+						type="textarea"
+						maxlength="150"
+						custom-class="has-fixed-size"
+						placeholder="Catatan singkat mengenai dokumen terkait."
+					>
+					</b-input>
+				</b-field>
+				<input type="hidden" name="projectId" v-model="projectId" />
+				<input type="hidden" name="period" v-model="delivery.periode" />
+				<button
+					class="button is-fullwidth is-success"
+					:disabled="disableSubmit"
+					type="submit"
+				>
+					Submit Document
+				</button>
+			</form>
+		</section>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		projectId: {
+			type: String,
+			required: true
+		},
+		action: {
+			type: String,
+			required: true
+		},
+		doctype: {
+			type: Array,
+			required: true
+		},
+		delivery: {
+			type: Object,
+			required: true
+		},
+		allowedFile: {
+			type: String,
+			required: true
+		}
+	},
+	data() {
+		return {
+			progress: null,
+			selectedDocument: null,
+			fileValidation: true
+		};
+	},
+	computed: {
+		documentName() {
+			if (this.selectedDocument !== null) {
+				let size = this.formatSizeString(this.selectedDocument.size);
+				this.checkExtention(this.selectedDocument.name);
+				return this.selectedDocument.name + " [ " + size + " ] ";
+			}
+
+			return "No Document Selected";
+		},
+		allowedArray() {
+			return this.allowedFile.split("|");
+		},
+		determineFieldStatus() {
+			if (!this.fileValidation) {
+				return "is-danger";
+			}
+
+			if (this.selectedDocument === null) {
+				return "";
+			}
+
+			return "is-success";
+		},
+		disableSubmit() {
+			if (!this.fileValidation || this.selectedDocument === null) {
+				return true;
+			}
+
+			return false;
+		}
+	},
+	methods: {
+		checkVersion() {
+			if (this.delivery.actual === null) {
+				return false;
+			}
+
+			return (
+				this.delivery.actual.version !== undefined ||
+				this.delivery.actual.version !== null ||
+				this.delivery.actual.version !== ""
+			);
+		},
+		checkExtention(filename) {
+			let sliced = filename.split(".");
+			let lastEnd = sliced.pop();
+			this.fileValidation = this.allowedArray.includes(
+				lastEnd.toLowerCase()
+			);
+		},
+		formatSizeString(size) {
+			let counter = 0;
+			while (size > 1000) {
+				size = size / 1000;
+				counter++;
+			}
+
+			if (counter == 0) {
+				size = size.toFixed(0) + " Byte";
+			} else if (counter == 1) {
+				size = size.toFixed(1) + " KB";
+			} else if (counter == 2) {
+				size = size.toFixed(1) + " MB";
+			} else if (counter == 3) {
+				size = size.toFixed(1) + " GB";
+			} else {
+				size = size.toFixed(2);
+			}
+
+			return size;
+		}
+	}
+};
+</script>
