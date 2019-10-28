7fa21e07ab2eef69acbeffa4d5faec757edd7162
adewiranataputra <adewiranataputra@gmail.com>
2019-10-28 17:25:59 +0700

    update timesheet, PM privilage, account, link

---
+++ src/development/scripts/profile/editProfile.js
@@ -0,0 +1,127 @@
+import Vue from 'vue';
+import Buefy from 'buefy'
+import { topNavBar } from "components";
+import Loader from 'helper-loader';
+import { crudInput } from 'components';
+import axios from 'Axios'
+import { notified } from "helper-tools";
+
+Vue.use(Buefy);
+new Vue({
+	el: '#contentApp',
+	components: {
+		topNavBar, crudInput
+	},
+	data: {
+		userlog: {
+			name: 'Ade Wiranat Putra'
+		},
+		selectedDocument: null,
+		allowedFile: "jpg|jpeg|png|doc|docx|pdf|xls|xlsx|ppt|pptx",
+		profile: true,
+		password: false
+	},
+	methods: {
+		checkExtention(filename) {
+			let sliced = filename.split(".");
+			let lastEnd = sliced.pop();
+			this.fileValidation = this.allowedArray.includes(
+				lastEnd.toLowerCase()
+			);
+		},
+		formatSizeString(size) {
+			let counter = 0;
+			while (size > 1000) {
+				size = size / 1000;
+				counter++;
+			}
+
+			if (counter == 0) {
+				size = size.toFixed(0) + " Byte";
+			} else if (counter == 1) {
+				size = size.toFixed(1) + " KB";
+			} else if (counter == 2) {
+				size = size.toFixed(1) + " MB";
+			} else if (counter == 3) {
+				size = size.toFixed(1) + " GB";
+			} else {
+				size = size.toFixed(2);
+			}
+
+			return size;
+		},
+		toggleEditPassword() {
+			this.password = true
+			this.profile = false
+		},
+		toggleEditProfile() {
+			this.profile = true
+			this.password = false
+		},
+		uploadFoto() {
+			let formData = new FormData();
+			let self = this
+
+			formData.append('file', this.selectedDocument);
+			console.log(this.selectedDocument)
+
+			axios.post('http://localhost:5501/demo/axios_check.php',
+				formData,
+				{
+					headers: {
+						'Content-Type': 'multipart/form-data'
+					}
+				}
+			).then(function (response) {
+				notified(self.$notification).success("Success");
+			})
+				.catch(function (error) {
+					notified(self.$notification).error(
+						"Mohon Maaf Kami Tidak dapat Terhubung dengan Server, Silakan Cek Koneksi Anda. Terimakasih 🙏"
+					);
+				});
+
+		},
+	},
+	computed: {
+		filterTaskName() {
+			return this.dataBaru.filter(option => {
+				return (
+					option.taskName
+						.toString()
+						.toLowerCase()
+						.indexOf(this.name.toLowerCase()) >= 0
+				);
+			});
+		},
+		documentName() {
+			if (this.selectedDocument !== null) {
+				let size = this.formatSizeString(this.selectedDocument.size);
+				this.checkExtention(this.selectedDocument.name);
+				return this.selectedDocument.name + " [ " + size + " ] ";
+			}
+
+			return "No Document Selected";
+		},
+		determineFieldStatus() {
+			if (!this.fileValidation) {
+				return "is-danger";
+			}
+
+			if (this.selectedDocument === null) {
+				return "";
+			}
+
+			return "is-success";
+		},
+		allowedArray() {
+			return this.allowedFile.split("|");
+		}
+	},
+	mounted() {
+		document.getElementById("editPassword").addEventListener("click", this.toggleEditPassword);
+		document.getElementById("editProfile").addEventListener("click", this.toggleEditProfile);
+		Loader.hide();
+
+	}
+});
