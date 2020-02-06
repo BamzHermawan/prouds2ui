12d119c24f4b80a8f51db01c77c934fd4ef4013d
Ario Widiatmoko <ariowidiatmoko@gmail.com>
2020-02-06 14:14:29 +0700

    Cleaning .. 🧹 * Separate Prouds Alignment file to an-extra * Add acivity type in timesheet approval adjustment

--- src/production/scripts/project/timesheet/adjustSubmission.vue
+++ src/production/scripts/project/timesheet/adjustSubmission.vue
@@ -1,71 +1,126 @@
 <template>
 	<form :action="action" method="post">
 		<input type="hidden" name="timesheet_id" :value="timesheetId" />
+		<label class="label">Adjustment Form</label>
+		<b-field grouped class="is-marginless">
+			<b-field>
+				<div class="field has-addons">
+					<p class="control">
+						<span
+							class="button is-normal-small is-static is-light-blend"
+							>Working Progress</span
+						>
+					</p>
+					<template v-if="active">
+						<p class="control">
+							<input
+								style="width:52px;"
+								v-model="modelProgress"
+								class="input is-small"
+								name="work_progress"
+								type="number"
+								max="100"
+								min="0"
+							/>
+						</p>
+						<p class="control">
+							<span class="button is-normal-small is-static"
+								>%</span
+							>
+						</p>
+					</template>
+					<p v-else class="control">
+						<span
+							class="button is-normal-small is-static"
+							style="border-style:none;"
+							>{{ modelProgress }} %</span
+						>
+					</p>
+				</div>
+			</b-field>
+			<b-field>
+				<div class="field has-addons">
+					<p class="control">
+						<span
+							class="button is-normal-small is-static is-light-blend"
+							>Work Hour</span
+						>
+					</p>
+					<template v-if="active">
+						<p class="control">
+							<input
+								style="width:52px;"
+								class="input is-small"
+								v-model="modelHour"
+								:disabled="!active"
+								type="number"
+								name="hour"
+								max="24"
+								min="1"
+							/>
+						</p>
+						<p class="control">
+							<span class="button is-normal-small is-static"
+								>Hours</span
+							>
+						</p>
+					</template>
+					<p v-else class="control">
+						<span
+							class="button is-normal-small is-static"
+							style="border-style:none;"
+							>{{ modelHour }} Hours</span
+						>
+					</p>
+				</div>
+			</b-field>
+		</b-field>
 		<b-field grouped>
-			<b-field label="Working Progress" style="width:125px;">
+			<b-field>
 				<div class="field has-addons">
 					<p class="control">
-						<input
-							v-model="modelProgress"
-							class="input is-small"
-							name="work_progress"
-							:disabled="!active"
-							type="number"
-							max="100"
-							min="0"
-						/>
+						<span
+							class="button is-normal-small is-static is-light-blend"
+						>
+							Activity Type
+						</span>
 					</p>
 					<p class="control">
-						<span class="button is-normal-small is-static">%</span>
+						<b-select
+							size="is-small"
+							v-model="model.type"
+							:disabled="!active"
+							name="activity_type"
+						>
+							<slot name="type-option"></slot>
+						</b-select>
 					</p>
 				</div>
 			</b-field>
-			<b-field label="Work Hour" style="width:125px;">
-				<div class="field has-addons">
-					<p class="control">
-						<input
-							class="input is-small"
-							v-model="modelHour"
-							:disabled="!active"
-							type="number"
-							name="hour"
-							max="24"
-							min="1"
-						/>
-					</p>
-					<p class="control">
-						<span class="button is-normal-small is-static"
-							>Hours</span
-						>
-					</p>
-				</div>
-			</b-field>
-			<b-field class="has-align-bottom" expanded>
-				<div class="field is-align-bottom" style="width:100%;">
-					<div class="buttons">
-						<b-button
-							v-show="!active"
-							size="is-normal-small"
-							@click="toggle"
-							type="is-info"
-							>Adjust</b-button
-						>
+			<b-field expanded>
+				<div class="buttons">
+					<b-button
+						v-show="!active"
+						size="is-normal-small"
+						@click="toggle"
+						type="is-info"
+						>Adjust</b-button
+					>
 
-						<b-button
-							v-show="active"
-							size="is-normal-small"
-							@click="toggle"
-							type="is-danger"
-							>Cancel</b-button
-						>
-						<b-button
-							native-type="submit"
-							v-show="active"
-							size="is-normal-small"
-							type="is-success"
-							>Save</b-button
-						>
-					</div>
+					<b-button
+						v-show="active"
+						size="is-normal-small"
+						@click="toggle"
+						type="is-danger"
+						>Cancel</b-button
+					>
+					<b-button
+						native-type="submit"
+						v-show="active"
+						size="is-normal-small"
+						type="is-success"
+						>Save</b-button
+					>
 				</div>
 			</b-field>
 		</b-field>
@@ -90,13 +145,18 @@ export default {
 		hour: {
 			type: Number,
 			required: true
+		},
+		activityType: {
+			type: Object,
+			required: true
 		}
 	},
 	data() {
 		return {
 			model: {
 				progress: this.progress,
-				hour: this.hour
+				hour: this.hour,
+				type: this.activityType.id
 			},
 			active: false
 		};
@@ -139,6 +199,7 @@ export default {
 		cancel() {
 			this.model.progress = this.progress;
 			this.model.hour = this.hour;
+			this.model.type = this.activityType.id;
 		}
 	}
 };
